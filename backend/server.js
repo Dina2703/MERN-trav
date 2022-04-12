77af42d06af51edee65eb49ac3ddfc034dc6400f
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-12 13:42:51 -0400

    setup express

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,9 @@
 // npm i -D nodemon   - as a dev dependency
 
 console.log("Hello");
+const express = require("express");
+const dotenv = require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.listen(port, () => console.log(`Server started on port ${port}`));
