96324664ac9d26a210c624c7b0fa82609aabee23
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-14 13:24:28 -0400

    JWT auth: User modal, add user to goal model

--- backend/models/goalModel.js
+++ backend/models/goalModel.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose");
 
+//add 'user' props to Goal schema, it's allows us to have a user associated with a goal.
+
 const goalSchema = mongoose.Schema(
   {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+      ref: "User",
+    },
     text: {
       type: String,
       require: [true, "Please add a text value"],
