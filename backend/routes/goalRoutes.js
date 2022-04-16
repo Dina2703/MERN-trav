09d38dd9c1c8aef26ef51f134718b57c7ab38c98
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-15 20:31:35 -0400

    Set and Get only Users goal

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -8,17 +8,19 @@ const {
   deleteGoals,
 } = require("../controllers/goalController");
 
+const { protect } = require("../middleware/authMiddleware");
+
 //GET request
-router.get("/", getGoals);
+router.get("/", protect, getGoals);
 
 //POST request
-router.post("/", setGoals);
+router.post("/", protect, setGoals);
 
 //PUT request
-router.put("/:id", updateGoals);
+router.put("/:id", protect, updateGoals);
 
 //DELETE request
-router.delete("/:id", deleteGoals);
+router.delete("/:id", protect, deleteGoals);
 
 //short version for CRUD Api, instead of the writing single line for each request, we can combine them by a route.
 // router.route("/").get(getGoals).post(setGoals);
