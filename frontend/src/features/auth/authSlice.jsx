b3f19f60d60730733a74d5c0d392dc5b3ba3f73d
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-17 11:35:29 -0400

    Start User registration

--- frontend/src/features/auth/authSlice.jsx
+++ frontend/src/features/auth/authSlice.jsx
@@ -1,4 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import authService from "./authService";
 
 //Get user from localStorage.
 //JSON.parse(string) converts JSON String to Object. Remember localStorage can only have strings so we want to parse it, then save the object into 'user'.
@@ -11,6 +12,25 @@ const initialState = {
   isLoading: false,
   message: "",
 };
+
+//Register user
+export const register = createAsyncThunk(
+  "auth/register",
+  async (user, thunkAPI) => {
+    try {
+      return await authService.register(user);
+    } catch (error) {
+      //errors from the server.if any of these errors exist it will put into 'message'
+      const message =
+        (error.response &&
+          error.response.data &&
+          error.response.data.message) ||
+        error.message ||
+        error.toString();
+      return thunkAPI.rejectWithValue(message);
+    }
+  }
+);
 
 export const authSlice = createSlice({
   name: "auth",
@@ -29,3 +49,5 @@ export const authSlice = createSlice({
 export const { reset } = authSlice.actions;
 
 export default authSlice.reducer;
+
+//async thunk function will deal with asynchronous data,  it's gonna deal with our backend.
