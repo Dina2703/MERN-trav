bea3387f61244e7f179a9b66b5e252bf2c3cc5e8
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-18 10:35:12 -0400

    Create goal extra reducer

--- frontend/src/features/goals/goalSlice.js
+++ frontend/src/features/goals/goalSlice.js
@@ -10,8 +10,9 @@ const initialState = {
 };
 
 //Create new goal
-export const createGoal = () =>
-  createAsyncThunk("goals/create", async (goalData, thunkAPI) => {
+export const createGoal = createAsyncThunk(
+  "goals/create",
+  async (goalData, thunkAPI) => {
     try {
       //we need to pass user token to createGoal(), for that we used thunkAPI, which has getState() method, that can get access to any part of a state. And token is inside 'user' part of the state.
       const token = thunkAPI.getState().auth.user.token;
@@ -26,13 +27,30 @@ export const createGoal = () =>
       //we access via action.payload for error message
       return thunkAPI.rejectWithValue(message);
     }
-  });
+  }
+);
 
 export const goalSlice = createSlice({
   name: "goal",
   initialState,
   reducers: {
     reset: (state) => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createGoal.pending, (state) => {
+        state.isLoading = true;
+      })
+      .addCase(createGoal.fulfilled, (state, action) => {
+        state.isLoading = false;
+        state.isSuccess = true;
+        state.goals.push(action.payload);
+      })
+      .addCase(createGoal.rejected, (state, action) => {
+        state.isLoading = false;
+        state.isError = true;
+        state.message = action.payload;
+      });
   },
 });
 
