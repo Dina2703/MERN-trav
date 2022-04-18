e5824c4aab6e86b09caa5f42c04b37f7a6fc3515
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-18 08:04:56 -0400

    small backend fix

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -71,15 +71,8 @@ const loginUser = asyncHandler(async (req, res) => {
 //@route GET /api/users/me
 //@access Private
 const getMe = asyncHandler(async (req, res) => {
-  //since we set user id in middleware, now we have access to it.
-  const { _id, name, email } = await User.findById(req.user.id);
-
   //when user logged in and they hit this route they should get their own info
-  res.status(200).json({
-    id: _id,
-    name,
-    email,
-  });
+  res.status(200).json(req.user);
 });
 
 //Generate JWT
