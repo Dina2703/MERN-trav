839d52a5f2ce02ef670b725bafee791dcd7a7aaa
Dinara Idrissova <Dinara.Idrissova@list.ru>
2022-04-19 09:25:18 -0400

    Prepare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 //npm i express dotenv mongoose colors   -install dependencies packages
 // npm i -D nodemon   - as a dev dependency
+
+const path = require("path");
 const colors = require("colors");
-console.log("Hello");
+// console.log("Hello");
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -20,6 +22,29 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/goals", require("./routes/goalRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
 
+//Serve frontend
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(
+      path.resolve(__dirname, "../", "frontend", "build", "index.html")
+    )
+  );
+} else {
+  app.get("/", (req, res) => res.send("Please set to production"));
+}
+
 app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
+
+//Prepare for deployment
+//if you do 'npm tun build' it then creates 'build' folder with all your static assets. So what we need to do is point to that  -->that's the static assets that index.html file that gets created in the build folder that is basically going to be the entry point to our frontend, so we have to add to this to the server.js file.
+// Make sure add it under the API routes. as we did under '//Serve frontend'
+// for 'production' mode: to point to our 'build' folder with our statis assets, do -->
+//app.use(express.static(path.join(_dirname, '../frontend/build')));
+//__dirname (double underscore dirname) - is the current directory, server.js
+// to load our index.html inside our static folder, we add -->
+//app.get("*", (req, res) =>
+//res.sendFile(path.resolve(_dirname, "../", "frontend", "build", "index.html")));
